0b48240c26f52ee46671956f88e34d54d5f0a84b
Cicero Oliverio <cicero_oliverio@hotmail.com>
2019-04-14 22:22:28 -0300

    Select with parameter in MongoDb

--- src/main/java/rc/springbootmongodb/controller/HotelController.java
+++ src/main/java/rc/springbootmongodb/controller/HotelController.java
@@ -1,7 +1,13 @@
 package rc.springbootmongodb.controller;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rc.springbootmongodb.entity.Hotel;
@@ -20,6 +26,40 @@ public class HotelController {
   @GetMapping("/all")
   public List<Hotel> getAll(){
     List<Hotel> hotels = this.hotelRepository.findAll();
+    return hotels;
+  }
+
+  @PutMapping
+  public void insert(@RequestBody Hotel hotel){
+    this.hotelRepository.insert(hotel);
+  }
+
+  @PostMapping
+  public void update(@RequestBody Hotel hotel){
+    this.hotelRepository.save(hotel);
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable("id") String id){
+    this.hotelRepository.deleteById(id);
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Hotel> getById(@PathVariable("id")String id){
+    Optional<Hotel> hotel = this.hotelRepository.findById(id);
+    return hotel;
+  }
+
+  @GetMapping("/price/{maxPrice}")
+  public List<Hotel> getPricePerNight(@PathVariable("maxPrice") int maxPrice){
+    List<Hotel> hotels = this.hotelRepository.findByPricePerNightLessThan(maxPrice);
+    return hotels;
+  }
+
+  @GetMapping("/address/{city}")
+  public List<Hotel> getByCity(@PathVariable("city") String city){
+    List<Hotel> hotels = this.hotelRepository.findByCity(city);
+
     return hotels;
   }
 
